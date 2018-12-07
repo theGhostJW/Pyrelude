779c8ecaa75349215b35d284d750fd10c49bef6e
theGhostJW <theghostjw@gmail.com>
2018-12-07 15:30:10 +1100

    added count function and wrapped basic boolean functions

--- src/Foundation/Extended.hs
+++ src/Foundation/Extended.hs
@@ -7,24 +7,27 @@ module Foundation.Extended (
   , module THEx
   , StringLike(..)
   , Truthy(..)
+  , count
 ) where
 
+import qualified Data.Bool                           as B
 import           Data.Either
 import qualified Data.Text                           as Text
 import           Debug.Trace.Extended
-import           Foundation
+import           Foundation                          hiding (not, (&&), (||))
+import           Foundation.Collection
 import           Foundation.Compat.Text              as Compat
 import           Foundation.Internal
 import           Language.Haskell.TH.Syntax.Extended as THEx (moduleOf)
 import           Path.Extended
 import           Path.IO.Extended                    as PathIO
-import qualified Prelude
+import qualified Prelude                             as P
 
   -- fromString clashes with isString typeclass
 class StringLike a where
   toStr :: a -> String
   fromStr :: String -> a
-  toPreludeStr :: a -> Prelude.String
+  toPreludeStr :: a -> P.String
   toText :: a -> Text.Text
 
 instance StringLike String where
@@ -39,11 +42,17 @@ instance StringLike Text.Text where
   toPreludeStr = Text.unpack
   toText = id
 
-instance StringLike Prelude.String where
+instance StringLike P.String where
   fromStr = toList
   toStr = fromList
   toPreludeStr = id
   toText = Text.pack
+
+-- redeclare to complement truthy
+{-# ANN otherwise "HLint: ignore" #-}
+otherwise :: Bool
+otherwise = True
+
 
 class Truthy b where
   isTruthy :: b -> Bool
@@ -52,5 +61,26 @@ class Truthy b where
   (?) :: b -> a -> a -> a
   (?) b a1 a2 = if isTruthy b then a1 else a2
 
+  -- and
+  infixr 3 &&
+  (&&) :: b -> b -> Bool
+  (&&) a b = isTruthy a B.&& isTruthy b
+
+  -- or
+  infixr 2 ||
+  (||) :: b -> b -> Bool
+  (||) a b = isTruthy a B.|| isTruthy b
+
+  -- not
+  not :: b -> Bool
+  not = B.not . isTruthy
+
+  -- reverse ternary operator
+  bool :: a -> a -> b -> a
+  bool fv tv b = B.bool fv tv $ isTruthy b
+
 instance Truthy Bool where
   isTruthy  = id
+
+count :: (Foldable collection, Truthy b, Additive a, P.Num a) => (Element collection -> b) -> collection -> a
+count p = foldl' (\n x -> p x ? n + 1 $ n) 0
--- test/FoundationTest.hs
+++ test/FoundationTest.hs
@@ -1,8 +1,9 @@
 
 module FoundationTest where
 
-import           Foundation.Extended
-import           Test.Extended
+import qualified Data.Bool                           as B
+import           Foundation.Extended as F
+import           Test.Extended as T
 
 genericStringLike g = property $ do
                         str <- forAll $ g (linear 0 100) ascii
@@ -17,6 +18,16 @@ hprop_stringLike_text = genericStringLike text
 hprop_stringLike_string :: Property
 hprop_stringLike_string = genericStringLike string
 
+unit_module_of = chkEq "FoundationTest" $ moduleOf ''StopSign
+
+--- Count ---
+unit_count_many = chkEq 4 $ count (== 5) [1, 2, 3, 5, 5, 6, 7, 5, 5]
+unit_count_last = chkEq 1 $ count (== 5) [1, 2, 3, 3, 2, 6, 7, 1, 5]
+unit_count_first = chkEq 1 $ count (== 5) [5, 2, 3, 3, 2, 6, 7, 1, 9]
+unit_count_zero = chkEq 0 $ count (== 5) [7, 2, 3, 3, 2, 6, 7, 1, 9]
+unit_count_empty = chkEq 0 $ count (== 5) []
+
+-- Truthy --
 unit_bool_ternary_true = chk $ (1 < 2) ? True $ False
 unit_bool_ternary_false = chkFalse $ (1 > 2) ? True $ False
 
@@ -28,4 +39,23 @@ instance Truthy StopSign where
 unit_ternary_custom_true = chk $ Go ? True $ False
 unit_ternary_custom_false = chkFalse $ Stop ? True $ False
 
-unit_module_of = chkEq "FoundationTest" $ moduleOf ''StopSign
+boolCheck ft fb = property $ do
+                         b1 <- forAll T.bool
+                         b2 <- forAll T.bool
+                         ft b1 b2 === fb b1 b2
+
+boolCheckSingle ft fb = property $ do
+                         b <- forAll T.bool
+                         ft b === fb b
+
+hprop_truty_and :: Property
+hprop_truty_and = boolCheck (B.&&) (&&)
+
+hprop_truty_or :: Property
+hprop_truty_or = boolCheck (B.||)  (||)
+
+hprop_truty_bool :: Property
+hprop_truty_bool = boolCheckSingle (B.bool 1 2) (F.bool 1 2)
+
+hprop_truty_not :: Property
+hprop_truty_not = boolCheckSingle B.not F.not
