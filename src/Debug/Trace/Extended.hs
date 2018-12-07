2ce8a716c14b4dabc8fc022054b0b74eb81626e0
theGhostJW <theghostjw@gmail.com>
2018-12-08 09:48:30 +1100

    added debugf and debugf'

--- src/Debug/Trace/Extended.hs
+++ src/Debug/Trace/Extended.hs
@@ -16,3 +16,9 @@ debug x = traceStr ("DEBUG: " <> show x) x
 
 debug' :: Show a => String -> a -> a
 debug' name x = traceStr ("DEBUG: " <> name <> ": " <> show x) x
+
+debugf :: Show b => (a -> b) -> a -> a
+debugf shower x = traceStr ("DEBUG: " <> show (shower x)) x
+
+debugf' :: Show b => (a -> b) -> String -> a -> a
+debugf' shower name x = traceStr ("DEBUG: " <> name <> ": " <> show (shower x)) x
