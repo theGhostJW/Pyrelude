e0c3d74d48eb27c6bab3ea628535bdeeb3d2e1bd
theGhostJW <theghostjw@gmail.com>
2018-12-13 13:37:27 +1100

    added System.IO.Silently to Test

--- src/Test/Extended.hs
+++ src/Test/Extended.hs
@@ -1,9 +1,11 @@
 
 module Test.Extended (
-  module Test.Tasty.HUnit.Extended
-  , module Test.Hedgehog.Extended
+  module Test.Tasty.HUnit.Extended,
+  module Test.Hedgehog.Extended,
+  module System.IO.Silently
 
 ) where
 
+import           System.IO.Silently
 import           Test.Hedgehog.Extended
 import           Test.Tasty.HUnit.Extended
