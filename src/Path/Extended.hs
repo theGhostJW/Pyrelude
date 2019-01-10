6489ae4c475bc0a84622c48b1a0943d52a38a9c5
theGhostJW <theghostjw@gmail.com>
2019-01-11 10:08:09 +1100

    Added path type Aliases

--- src/Path/Extended.hs
+++ src/Path/Extended.hs
@@ -3,7 +3,11 @@ module Path.Extended (
   parseRelDirSafe,
   parseAbsDirSafe,
   parseRelFileSafe,
-  parseAbsFileSafe
+  parseAbsFileSafe,
+  AbsDir,
+  RelDir,
+  RelFile,
+  AbsFile
 ) where
 
 import qualified Control.Monad.Catch                     as C
@@ -11,17 +15,22 @@ import           Foundation.Extended.Internal.StringLike
 import           Path
 import           Prelude                                 as P
 
+type AbsDir = Path Abs Dir
+type RelDir = Path Rel Dir
+type RelFile = Path Rel File
+type AbsFile = Path Abs File
+
 safeParsePath :: (C.MonadCatch m, StringLike s) => (P.String -> m (Path ra df) ) -> s -> m (Either PathException (Path ra df))
 safeParsePath parser subDirName = C.try $ parser $ toCharList subDirName
 
-parseRelDirSafe :: (C.MonadCatch m, StringLike s) => s -> m (Either PathException (Path Rel Dir))
+parseRelDirSafe :: (C.MonadCatch m, StringLike s) => s -> m (Either PathException RelDir)
 parseRelDirSafe = safeParsePath parseRelDir
 
-parseAbsDirSafe :: (C.MonadCatch m, StringLike s) => s -> m (Either PathException (Path Abs Dir))
+parseAbsDirSafe :: (C.MonadCatch m, StringLike s) => s -> m (Either PathException AbsDir)
 parseAbsDirSafe = safeParsePath parseAbsDir
 
-parseRelFileSafe :: (C.MonadCatch m, StringLike s) => s -> m (Either PathException (Path Rel File))
+parseRelFileSafe :: (C.MonadCatch m, StringLike s) => s -> m (Either PathException RelFile)
 parseRelFileSafe = safeParsePath parseRelFile
 
-parseAbsFileSafe :: (C.MonadCatch m, StringLike s) => s -> m (Either PathException (Path Abs File))
+parseAbsFileSafe :: (C.MonadCatch m, StringLike s) => s -> m (Either PathException AbsFile)
 parseAbsFileSafe = safeParsePath parseAbsFile
