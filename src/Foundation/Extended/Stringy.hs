9fcb080c612a01b8c0792dd91b2f0ff5179b517d
theGhostJW <theghostjw@gmail.com>
2019-01-25 06:26:27 +1100

    addded toS to Stringy

--- src/Foundation/Extended/Stringy.hs
+++ src/Foundation/Extended/Stringy.hs
@@ -4,30 +4,34 @@ module Foundation.Extended.Stringy (
 
 import qualified Data.Text              as Text
 import           Foundation
-import           Foundation.Compat.Text as C
+import qualified Foundation.Compat.Text as C
 import qualified Prelude                as P
 
   -- fromString clashes with isString typeclass
 class Stringy a where
-  toStr :: a -> String
   fromStr :: String -> a
+  toStr :: a -> String
+  toS :: Stringy b => b -> a
   toCharList :: a -> P.String
   toText :: a -> Text.Text
 
 instance Stringy String where
   fromStr = id
   toStr = id
+  toS = toStr
   toCharList = toList
   toText = C.toText
 
 instance Stringy Text.Text where
   fromStr = C.toText
-  toStr = fromText
+  toStr = C.fromText
+  toS = toText
   toCharList = Text.unpack
   toText = id
 
 instance Stringy P.String where
   fromStr = toList
   toStr = fromList
+  toS = toCharList
   toCharList = id
   toText = Text.pack
