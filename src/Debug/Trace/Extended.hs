9e4ec66e189c307ef0583772a60201a3ebd87bd6
theGhostJW <theghostjw@gmail.com>
2019-02-28 11:48:53 +1100

    added debugPrint

--- src/Debug/Trace/Extended.hs
+++ src/Debug/Trace/Extended.hs
@@ -5,22 +5,23 @@ module Debug.Trace.Extended (
   , debug'
   , debugf
   , debugf'
+  , debugPrint
 ) where
 
 import           Debug.Trace
 import           Foundation
 
-traceStr :: String -> a -> a
-traceStr = trace . toList
+debugPrint :: String -> a -> a
+debugPrint s = trace (toList ("DEBUG: " <> s))
 
 debug :: Show a => a -> a
-debug x = traceStr ("DEBUG: " <> show x) x
+debug x = debugPrint (show x) x
 
 debug' :: Show a => String -> a -> a
-debug' name x = traceStr ("DEBUG: " <> name <> ": " <> show x) x
+debug' name x = debugPrint (name <> ": " <> show x) x
 
 debugf :: Show b => (a -> b) -> a -> a
-debugf shower x = traceStr ("DEBUG: " <> show (shower x)) x
+debugf shower x = debugPrint (show (shower x)) x
 
 debugf' :: Show b => (a -> b) -> String -> a -> a
-debugf' shower name x = traceStr ("DEBUG: " <> name <> ": " <> show (shower x)) x
+debugf' shower name x = debugPrint (name <> ": " <> show (shower x)) x
