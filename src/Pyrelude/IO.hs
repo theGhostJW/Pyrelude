8a6dc5e68273d380e54aaed0b6bbad85e0686bd2
theGhostJW <theghostjw@gmail.com>
2020-05-10 07:38:35 +1000

    added Data.Time

--- src/Pyrelude/IO.hs
+++ src/Pyrelude/IO.hs
@@ -2,8 +2,10 @@ module Pyrelude.IO (
     module Path.IO.Extended
   , module Data.Text.IO
   , module Chronos
+  , module Data.Time
 ) where
 
-import           Path.IO.Extended
+import Path.IO.Extended
 import Data.Text.IO
+import Data.Time (getCurrentTimeZone)
 import Chronos (today, tomorrow, yesterday, now, todayDayOfWeek, yesterdayDayOfWeek, stopwatch, stopwatch_)
