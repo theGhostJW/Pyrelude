4c7196461914f8cab994356f9359e00e76a1c842
theGhostJW <theghostjw@gmail.com>
2022-08-27 18:28:51 +1000

    fix missing export

--- src/Pyrelude/Test/Tasty/HUnit/Extended.hs
+++ src/Pyrelude/Test/Tasty/HUnit/Extended.hs
@@ -1,53 +1,69 @@
+module Pyrelude.Test.Tasty.HUnit.Extended
+  ( module HUnit,
+    safeLoad,
+    chkEq,
+    chkEq',
+    (...),
+    chkContains,
+    chk,
+    chk',
+    chkFail,
+    chkFalse,
+    chkLeft,
+    chkLeftContains',
+    chkLeftContains,
+  )
+where
 
-module Pyrelude.Test.Tasty.HUnit.Extended (
-  module HUnit
-  , safeLoad
-  , chkEq
-  , (...)
-  , chkContains
-  , chk
-  , chk'
-  , chkFail
-  , chkFalse
-  , chkLeft
-  , chkLeftContains'
-  , chkLeftContains
-) where
-
+import BasePrelude
+import Control.Exception.Base
+import Control.Monad
 import Data.Text as T
-import           Control.Exception.Base
-import           Control.Monad
-import           BasePrelude
-import           Stringy
-import qualified Prelude as P
-import           Test.Tasty.HUnit       as HUnit hiding (Assertable,
-                                                  AssertionPredicate, assert,
-                                                  assertString)
+import Stringy
+import Test.Tasty.HUnit as HUnit hiding
+  ( Assertable,
+    AssertionPredicate,
+    assert,
+    assertString,
+  )
+import Prelude qualified as P
 
-
-safeLoad :: (Exception e) => (s -> IO a)  -> (Either e a -> Assertion) -> s -> Assertion
+safeLoad :: (Exception e) => (s -> IO a) -> (Either e a -> Assertion) -> s -> Assertion
 safeLoad stringLoader eitherConverter inputStr = eitherConverter =<< Control.Exception.Base.try (stringLoader inputStr)
 
-chkFail :: ConvertString s P.String  => s -> Assertion
+chkFail :: ConvertString s P.String => s -> Assertion
 chkFail = assertFailure . toS
 
 chkEq :: (Eq a, Show a) => a -> a -> Assertion
 chkEq = (@=?)
 
+chkEq' ::
+  (Eq a, Show a, HasCallStack) =>
+  -- | The message prefix
+  Text ->
+  -- | The expected value
+  a ->
+  -- | The actual value
+  a ->
+  Assertion
+chkEq' t = assertEqual (toS t)
 
 infixr 0 ...
-(...) :: (Eq a, Show a)
-                        => a -- ^ The expected value
-                        -> a -- ^ The actual value
-                        -> Assertion
+
+(...) ::
+  (Eq a, Show a) =>
+  -- | The expected value
+  a ->
+  -- | The actual value
+  a ->
+  Assertion
 (...) = (@=?)
 
 chkContains :: ConvertString s Text => s -> s -> Assertion
-chkContains needle hayStack = let
-                                needleStr = toS needle
-                                hayStackStr = toS hayStack
-                              in
-                                chk' (toS $ "substring not found: " <> needleStr <> "\n<<<IN>>>\n" <> hayStackStr) $ needleStr `T.isInfixOf` (hayStackStr :: Text)
+chkContains needle hayStack =
+  let needleStr = toS needle
+      hayStackStr = toS hayStack
+   in chk' (toS $ "substring not found: " <> needleStr <> "\n<<<IN>>>\n" <> hayStackStr) $ needleStr `T.isInfixOf` (hayStackStr :: Text)
 
 chk :: Bool -> Assertion
 chk = assertBool "check failed"
@@ -63,20 +79,18 @@ chkLeftContains = chkLeftContains' show
 
 chkLeftContains' :: forall r s s1 l. (Show r, ConvertString s Text, ConvertString s1 Text) => (l -> s1) -> s -> Either l r -> Assertion
 chkLeftContains' leftToStr expectedText eth =
-  let 
-    expected :: Text
-    expected = toS expectedText
-    
-    errStr :: l -> Text
-    errStr = toS . leftToStr
-  in
-    either 
-      (chkContains  expected . errStr)
-      (\actual -> chkFail $ "Error expected but no error generated. Actual is: " <> show actual)
-      eth
+  let expected :: Text
+      expected = toS expectedText
+
+      errStr :: l -> Text
+      errStr = toS . leftToStr
+   in either
+        (chkContains expected . errStr)
+        (\actual -> chkFail $ "Error expected but no error generated. Actual is: " <> show actual)
+        eth
 
 chkLeft :: Show r => (l -> Bool) -> Either l r -> Assertion
 chkLeft leftPred =
-  either 
+  either
     (chk . leftPred)
     (\actual -> chkFail $ "Error expected but no error generated. Actual is: " <> show actual)
