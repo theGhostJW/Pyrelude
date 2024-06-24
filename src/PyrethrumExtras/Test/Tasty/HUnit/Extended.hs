ef57662f6b6e6789ec5181e7c3e51461cfae1928
theGhostJW <theghostjw@gmail.com>
2024-06-24 21:15:50 +1000

    restored correct imports

--- src/PyrethrumExtras/Test/Tasty/HUnit/Extended.hs
+++ src/PyrethrumExtras/Test/Tasty/HUnit/Extended.hs
@@ -23,6 +23,10 @@ import Test.Tasty.HUnit as HUnit hiding
     assertString,
   )
 import Prelude qualified as P
+import Data.String.Encode 
+import Data.Text qualified as T
+import Control.Exception.Base qualified
+import Stringy (toS)
 
 safeLoad :: (Exception e) => (s -> IO a) -> (Either e a -> Assertion) -> s -> Assertion
 safeLoad stringLoader eitherConverter inputStr = eitherConverter =<< Control.Exception.Base.try (stringLoader inputStr)
@@ -36,7 +40,7 @@ chkEq = (@=?)
 chkEq' ::
   (Eq a, Show a, HasCallStack) =>
   -- | The message prefix
-  Text ->
+  T.Text ->
   -- | The expected value
   a ->
   -- | The actual value
@@ -55,30 +59,30 @@ infixr 0 ...
   Assertion
 (...) = (@=?)
 
-chkContains :: ConvertString s Text => s -> s -> Assertion
+chkContains :: ConvertString s T.Text => s -> s -> Assertion
 chkContains needle hayStack =
   let needleStr = toS needle
       hayStackStr = toS hayStack
-   in chk' (toS $ "substring not found: " <> needleStr <> "\n<<<IN>>>\n" <> hayStackStr) $ needleStr `T.isInfixOf` (hayStackStr :: Text)
+   in chk' (toS $ "substring not found: " <> needleStr <> "\n<<<IN>>>\n" <> hayStackStr) $ needleStr `T.isInfixOf` (hayStackStr :: T.Text)
 
 chk :: Bool -> Assertion
 chk = assertBool "check failed"
 
-chk' :: Text -> Bool -> Assertion
+chk' :: T.Text -> Bool -> Assertion
 chk' errMsg = assertBool $ toS errMsg
 
 chkFalse :: Bool -> Assertion
 chkFalse condition = chk $ not condition
 
-chkLeftContains :: (Show r, Show l, ConvertString s Text) => s -> Either l r -> Assertion
+chkLeftContains :: (Show r, Show l, ConvertString s T.Text ) => s -> Either l r -> Assertion
 chkLeftContains = chkLeftContains' show
 
-chkLeftContains' :: forall r s s1 l. (Show r, ConvertString s Text, ConvertString s1 Text) => (l -> s1) -> s -> Either l r -> Assertion
+chkLeftContains' :: forall r s s1 l. (Show r, ConvertString s T.Text , ConvertString s1 T.Text ) => (l -> s1) -> s -> Either l r -> Assertion
 chkLeftContains' leftToStr expectedText eth =
-  let expected :: Text
+  let expected :: T.Text 
       expected = toS expectedText
 
-      errStr :: l -> Text
+      errStr :: l -> T.Text 
       errStr = toS . leftToStr
    in either
         (chkContains expected . errStr)
